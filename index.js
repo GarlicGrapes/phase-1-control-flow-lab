ab792e4152aaafd0d59bd2febe7e1ddfdac9b4db
Austin <austinlongocontracting@gmail.com>
2021-11-30 17:22:09 -0500

    commit tree 037d00d53c2bc572a1f4b2f4ebde166555b67909

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  if (rideLength < 400) {
+    return 'This one is on me!';
+  }
+  else if (rideLength > 2500) {
+    return 'No can do.';
+  }
+  else if (rideLength > 2000){
+    return 'I will gladly take your thirty bucks.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city !== "NYC") {
+    return "No go."
+  }
+  else {
+    return "Ok, sounds good."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip == 'generous') {
+    return "Thank you so much.";
+  }
+  else if (tip == 'not as generous') {
+    return "Thank you.";
+  }
+  else {
+    return "Bye.";
+  }
 }
